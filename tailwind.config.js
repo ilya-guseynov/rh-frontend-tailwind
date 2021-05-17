95692daedef992e67517b32fe5b402c15d1f3a85
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-05-17 19:06:51 +0300

    new: removed tw prefix and added custom container component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: "tw-",
+  prefix: "",
   purge: {
     enabled: true,
     content: ["./src/**/*.html"],
@@ -11,5 +11,43 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1200px',
+          }
+        },
+        '.container--header': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1200px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1600px',
+          }
+        }
+      })
+    }
+  ],
 };
