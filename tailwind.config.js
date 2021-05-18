4387efc1b3a3260b94c3111f20913ceb10cf220a
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-05-18 14:59:27 +0300

    new: b2b-menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,37 +14,49 @@ module.exports = {
   plugins: [
     function ({ addComponents }) {
       addComponents({
-        '.container': {
-          maxWidth: '100%',
-          '@screen sm': {
-            maxWidth: '640px',
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "640px",
           },
-          '@screen md': {
-            maxWidth: '768px',
+          "@screen md": {
+            maxWidth: "768px",
           },
-          '@screen lg': {
-            maxWidth: '1024px',
+          "@screen lg": {
+            maxWidth: "1024px",
           },
-          '@screen xl': {
-            maxWidth: '1200px',
+          "@screen xl": {
+            maxWidth: "1200px",
           }
         },
-        '.container--header': {
-          maxWidth: '100%',
-          '@screen sm': {
-            maxWidth: '640px',
+        ".container--header": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "640px",
           },
-          '@screen md': {
-            maxWidth: '768px',
+          "@screen md": {
+            maxWidth: "768px",
           },
-          '@screen lg': {
-            maxWidth: '1024px',
+          "@screen lg": {
+            maxWidth: "1024px",
           },
-          '@screen xl': {
-            maxWidth: '1200px',
+          "@screen xl": {
+            maxWidth: "1200px",
           },
-          '@screen 2xl': {
-            maxWidth: '1600px',
+          "@screen 2xl": {
+            maxWidth: "1600px",
+          }
+        },
+        ".rh-bg-gold": {
+          background: "linear-gradient(89.36deg, #D8BB58 -53.59%, #ECD26E -33.86%, #F4DB76 -21.39%, #E9CC65 4.9%, #CEA53A 56.52%, #C99F33 64.39%)"
+        },
+        ".rh-bg-green": {
+          backgroundColor: "#2A5D2E"
+        },
+        ".b2b-menu-shadow": {
+          boxShadow: "0px 2px 34px rgba(29, 37, 45, 0.1)",
+          "@screen md": {
+            boxShadow: "none"
           }
         }
       })
