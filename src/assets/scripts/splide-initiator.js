ae4e597bfd1a9a4a299d8145e4cf75794fd7364a
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-06-07 15:13:22 +0300

    new: make main b2b page link on prod

--- src/assets/scripts/splide-initiator.js
+++ src/assets/scripts/splide-initiator.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
   const sliderElementQuery = ".with-you-slider";
 
-  if (document.querySelector("sliderElementQuery")) {
+  if (document.querySelector(sliderElementQuery)) {
     new Splide(sliderElementQuery, {
       type: "loop",
       pagination: false
