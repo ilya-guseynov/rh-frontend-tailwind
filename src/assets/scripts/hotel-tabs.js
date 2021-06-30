22ba6e36c6dc6ac0d2460c66c12f0f796870ccbe
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-06-30 16:27:44 +0300

    new: realized zoom photo on nomer slider for 1 nomer

--- src/assets/scripts/hotel-tabs.js
+++ src/assets/scripts/hotel-tabs.js
@@ -19,6 +19,8 @@
       document.querySelectorAll(nomerElementQuery).forEach(nomer => {
         const sliderDesktopElementQuery = ".HotelPage__NomerPhotoSlider";
         const sliderMobileElementQuery = ".HotelPage__NomerMobilePhotoSlider";
+        const sliderPopupElementQuery = ".HotelPage__NomerPopupPhotoSlider--Desktop";
+        const sliderPopupMobileElementQuery = ".HotelPage__NomerPopupPhotoSlider--Mobile";
 
         const splideDesktop = new Splide(nomer.querySelector(sliderDesktopElementQuery), {
           type: "loop",
@@ -28,7 +30,7 @@
           direction: "ttb",
           height: "675px",
           pagination: false,
-          lazyLoad: "nearby",
+          lazyLoad: "sequential",
           arrowPath: "M2.0162 39.9541C3.37923 41.3486 5.58056 41.3486 6.94359 39.9541L22.9838 23.4452C24.3387 22.0423 24.3387 19.7767 22.9838 18.3738L6.94359 1.86487C5.55707 0.64279 3.49025 0.724954 2.19945 2.05347C0.908654 3.38199 0.828821 5.50921 2.0162 6.93624L15.5752 20.9275L2.0162 34.8827C0.661266 36.2856 0.661266 38.5512 2.0162 39.9541Z"
         }).mount();
 
@@ -38,14 +40,48 @@
           perPage: 1,
           perMove: 1,
           pagination: false,
-          lazyLoad: "nearby",
+          lazyLoad: "sequential",
           preloadPages: 3,
           gap: "2em",
-          throttle: 10,
+          arrowPath: "M1.06039 38.9541C2.48267 40.3486 4.77972 40.3486 6.202 38.9541L22.9396 22.4452C24.3535 21.0423 24.3535 18.7767 22.9396 17.3738L6.202 0.864871C4.7552 -0.35721 2.59852 -0.275046 1.2516 1.05347C-0.0953188 2.38199 -0.178623 4.50921 1.06039 5.93624L15.209 19.9275L1.06039 33.8827C-0.353463 35.2856 -0.353463 37.5512 1.06039 38.9541Z"
+        }).mount();
+
+        const sliderPopup = new Splide(nomer.querySelector(sliderPopupElementQuery), {
+          type: "loop",
+          autoplay: true,
+          perPage: 1,
+          perMove: 1,
+          pagination: false,
+          lazyLoad: "sequential",
+          preloadPages: 3,
           arrowPath: "M1.06039 38.9541C2.48267 40.3486 4.77972 40.3486 6.202 38.9541L22.9396 22.4452C24.3535 21.0423 24.3535 18.7767 22.9396 17.3738L6.202 0.864871C4.7552 -0.35721 2.59852 -0.275046 1.2516 1.05347C-0.0953188 2.38199 -0.178623 4.50921 1.06039 5.93624L15.209 19.9275L1.06039 33.8827C-0.353463 35.2856 -0.353463 37.5512 1.06039 38.9541Z"
         });
 
-        sliderMobile.sync(splideDesktop).mount();
+        const sliderPopupMobile = new Splide(nomer.querySelector(sliderPopupMobileElementQuery), {
+          type: "loop",
+          autoplay: true,
+          perPage: 1,
+          perMove: 1,
+          pagination: false,
+          lazyLoad: "sequential",
+          preloadPages: 3,
+          arrowPath: "M1.06039 38.9541C2.48267 40.3486 4.77972 40.3486 6.202 38.9541L22.9396 22.4452C24.3535 21.0423 24.3535 18.7767 22.9396 17.3738L6.202 0.864871C4.7552 -0.35721 2.59852 -0.275046 1.2516 1.05347C-0.0953188 2.38199 -0.178623 4.50921 1.06039 5.93624L15.209 19.9275L1.06039 33.8827C-0.353463 35.2856 -0.353463 37.5512 1.06039 38.9541Z"
+        });
+
+        sliderPopup.sync(splideDesktop).mount();
+        sliderPopupMobile.sync(sliderMobile).mount();
+
+        sliderMobile.on("click", () => {
+          nomer.querySelector(".HotelPage__NomerPopupPhotoSliderContainer").classList.add("HotelPage__NomerPopupPhotoSliderContainer--ActiveMobile");
+        });
+        
+        splideDesktop.on("click", () => {
+          nomer.querySelector(".HotelPage__NomerPopupPhotoSliderContainer").classList.add("HotelPage__NomerPopupPhotoSliderContainer--ActiveDesktop");
+        });
+
+        nomer.querySelector(".HotelPage__NomerPopupScreenlock").addEventListener("click", () => {
+          nomer.querySelector(".HotelPage__NomerPopupPhotoSliderContainer").classList.remove("HotelPage__NomerPopupPhotoSliderContainer--ActiveMobile", "HotelPage__NomerPopupPhotoSliderContainer--ActiveDesktop");
+        });
       });
     }, 100);
   };
