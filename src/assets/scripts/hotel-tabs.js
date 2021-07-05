216c523879f2bbbb93c6586d2fcb54f1fc6d65e5
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-07-05 12:38:21 +0300

    new: removed mobile zoom on hotel page

--- src/assets/scripts/hotel-tabs.js
+++ src/assets/scripts/hotel-tabs.js
@@ -20,7 +20,6 @@
         const sliderDesktopElementQuery = ".HotelPage__NomerPhotoSlider";
         const sliderMobileElementQuery = ".HotelPage__NomerMobilePhotoSlider";
         const sliderPopupElementQuery = ".HotelPage__NomerPopupPhotoSlider--Desktop";
-        const sliderPopupMobileElementQuery = ".HotelPage__NomerPopupPhotoSlider--Mobile";
 
         const splideDesktop = new Splide(nomer.querySelector(sliderDesktopElementQuery), {
           type: "loop",
@@ -58,23 +57,7 @@
           arrowPath: "M1.06039 38.9541C2.48267 40.3486 4.77972 40.3486 6.202 38.9541L22.9396 22.4452C24.3535 21.0423 24.3535 18.7767 22.9396 17.3738L6.202 0.864871C4.7552 -0.35721 2.59852 -0.275046 1.2516 1.05347C-0.0953188 2.38199 -0.178623 4.50921 1.06039 5.93624L15.209 19.9275L1.06039 33.8827C-0.353463 35.2856 -0.353463 37.5512 1.06039 38.9541Z"
         });
 
-        const sliderPopupMobile = new Splide(nomer.querySelector(sliderPopupMobileElementQuery), {
-          type: "loop",
-          autoplay: true,
-          perPage: 1,
-          perMove: 1,
-          pagination: false,
-          lazyLoad: "nearby",
-          preloadPages: 3,
-          arrowPath: "M1.06039 38.9541C2.48267 40.3486 4.77972 40.3486 6.202 38.9541L22.9396 22.4452C24.3535 21.0423 24.3535 18.7767 22.9396 17.3738L6.202 0.864871C4.7552 -0.35721 2.59852 -0.275046 1.2516 1.05347C-0.0953188 2.38199 -0.178623 4.50921 1.06039 5.93624L15.209 19.9275L1.06039 33.8827C-0.353463 35.2856 -0.353463 37.5512 1.06039 38.9541Z"
-        });
-
         sliderPopup.sync(splideDesktop).mount();
-        sliderPopupMobile.sync(sliderMobile).mount();
-
-        sliderMobile.on("click", () => {
-          nomer.querySelector(".HotelPage__NomerPopupPhotoSliderContainer").classList.add("HotelPage__NomerPopupPhotoSliderContainer--ActiveMobile");
-        });
         
         splideDesktop.on("click", () => {
           nomer.querySelector(".HotelPage__NomerPopupPhotoSliderContainer").classList.add("HotelPage__NomerPopupPhotoSliderContainer--ActiveDesktop");
