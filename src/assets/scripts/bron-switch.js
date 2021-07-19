10bcd33bef01a128dab1627ee1155bd72a781e35
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-07-19 21:58:09 +0300

    new: bron

---
+++ src/assets/scripts/bron-switch.js
@@ -0,0 +1,27 @@
+(() => {
+  const bronSwtichTriggerButtons = document.querySelectorAll(".Bron__SwitchButton");
+  const bronContentBlocks = document.querySelectorAll(".Bron__Content");
+
+  if (!bronSwtichTriggerButtons || !bronContentBlocks) {
+    return;
+  }
+
+  bronSwtichTriggerButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      bronSwtichTriggerButtons.forEach(buttonToRemoveActiveStatus => {
+        buttonToRemoveActiveStatus.classList.remove("Bron__SwitchButton--Active");
+      });
+
+      button.classList.add("Bron__SwitchButton--Active");
+
+      bronContentBlocks.forEach(bronContentBlock => {
+        bronContentBlock.classList.remove("Bron__Content--Active");
+        if (bronContentBlock.getAttribute("bron-content") === button.getAttribute("bron-switch-trigger")) {
+          bronContentBlock.classList.add("Bron__Content--Active");
+        }
+      });
+    });
+  });
+
+  bronSwtichTriggerButtons[0].click();
+})();
