98837086a1bbd8b9484c35aad5ffa908acf70169
ilya-guseynov <ilya.guseynov.self@gmail.com>
2021-07-28 08:52:12 +0300

    new: add gulp task to watch and rebuild only html css js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const {src, dest, parallel, series, watch} = require("gulp")
-const fileInclude = require("gulp-file-include")
-const sourcemaps = require("gulp-sourcemaps")
-const postcss = require("gulp-postcss")
-const concat = require("gulp-concat")
-const babel = require("gulp-babel")
-const uglify = require("gulp-uglify")
-const exec = require('child_process').exec
-const browsersync = require("browser-sync")
-const paths = require("./paths.config")
+const {src, dest, parallel, series, watch} = require("gulp");
+const fileInclude = require("gulp-file-include");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const exec = require('child_process').exec;
+const browsersync = require("browser-sync");
+const paths = require("./paths.config");
 
 const html = () => src(paths.src.html)
   .pipe(fileInclude({
@@ -16,14 +16,14 @@ const html = () => src(paths.src.html)
     indent: true
   }))
   .pipe(dest(paths.build.html))
-  .pipe(browsersync.stream())
+  .pipe(browsersync.stream());
 
 const styles = () => src(paths.src.styles)
   .pipe(sourcemaps.init())
   .pipe(postcss())
   .pipe(sourcemaps.write())
   .pipe(dest(paths.build.styles))
-  .pipe(browsersync.stream())
+  .pipe(browsersync.stream());
 
 const scripts = () => src(paths.src.scripts)
   .pipe(sourcemaps.init())
@@ -34,18 +34,18 @@ const scripts = () => src(paths.src.scripts)
   .pipe(uglify())
   .pipe(sourcemaps.write())
   .pipe(dest(paths.build.scripts))
-  .pipe(browsersync.stream())
+  .pipe(browsersync.stream());
 
-const images = () => src(paths.src.images).pipe(dest(paths.build.images)).pipe(browsersync.stream())
+const images = () => src(paths.src.images).pipe(dest(paths.build.images)).pipe(browsersync.stream());
 
-const fonts = () => src(paths.src.fonts).pipe(dest(paths.build.fonts)).pipe(browsersync.stream())
+const fonts = () => src(paths.src.fonts).pipe(dest(paths.build.fonts)).pipe(browsersync.stream());
 
-const videos = () => src(paths.src.videos).pipe(dest(paths.build.videos)).pipe(browsersync.stream())
+const videos = () => src(paths.src.videos).pipe(dest(paths.build.videos)).pipe(browsersync.stream());
 
 const clean = done => {
-  exec("rm -rf ./build")
-  done()
-}
+  exec("rm -rf ./build");
+  done();
+};
 
 const browserSyncStart = done => {
   browsersync.init({
@@ -53,32 +53,35 @@ const browserSyncStart = done => {
       baseDir: "./build/"
     },
     port: 3000
-  })
-  done()
-}
+  });
+  done();
+};
 
 const browserSyncReload = done => {
-  browsersync.reload()
-  done()
-}
+  browsersync.reload();
+  done();
+};
 
-const build = parallel(html, styles, scripts, images, fonts, videos)
+const build = parallel(html, styles, scripts, images, fonts, videos);
 
-const watchFiles = () => watch("./src/**/*", series(build, browserSyncReload))
+const watchFiles = () => watch("./src/**/*", series(build, browserSyncReload));
 
 const watchSamoFiles = () => watch("./src/**/*", series(build, done => {
-  exec("cp ./build/assets/customers.css /home/bitrix/ext_www/search.resort-holiday.com/public/css/customer.css")
-  done()
-}))
+  exec("cp ./build/assets/customers.css /home/bitrix/ext_www/search.resort-holiday.com/public/css/customer.css");
+  done();
+}));
 
-const watchSrc = parallel(browserSyncStart, watchFiles)
+const watchNoStaticFiles = parallel(browserSyncStart, () => watch("./src/**/*", series(parallel(html, styles, scripts), browserSyncReload)));
 
-module.exports.html = html
-module.exports.styles = styles
-module.exports.scripts = scripts
-module.exports.images = images
-module.exports.fonts = fonts
-module.exports.videos = videos
-module.exports.build = build
-module.exports.watch = watchSrc
-module.exports.watchsamo = watchSamoFiles
+const watchSrc = parallel(browserSyncStart, watchFiles);
+
+module.exports.html = html;
+module.exports.styles = styles;
+module.exports.scripts = scripts;
+module.exports.images = images;
+module.exports.fonts = fonts;
+module.exports.videos = videos;
+module.exports.build = build;
+module.exports.watch = watchSrc;
+module.exports.watchsamo = watchSamoFiles;
+module.exports.watchnostat = watchNoStaticFiles;
